110902969595ccf41c1f892aba4f1bb09af93294
Danny Rosen <danny.rosen@gmail.com>
2014-02-01 17:33:47 -0500

    initial commit. v1.

---
+++ inline.js
@@ -0,0 +1,21 @@
+function loadDesign(design_id) {
+    if (design_id === "a")
+        pageToDisplay = "1.html";
+    else if (design_id === "b")
+        pageToDisplay = "2.html";
+    else if (design_id === "c")
+        pageToDisplay = "3.html";
+    var xmlhttp;
+    if (window.XMLHttpRequest) {
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById("contentArea").innerHTML = xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET", pageToDisplay, true);
+    xmlhttp.send();
+}
---
+++ js/functions.js
@@ -0,0 +1,24 @@
+    //Get's the sample from the server defined in sixpack.js, chooses the design based on the servers response 
+    function getSample() {
+        window.session = new sixpack.Session();
+        var d = new Date();
+        window.m = d.getMonth();
+        session.participate("month" + m, ['a', 'b', 'c'], function (err, res) {
+            if (err) throw err;
+            // for designs available 
+            var designs = ['a', 'b', 'c'];
+            for (var i = 0; i < designs.length; i++) {
+                if (res.alternative['name'] === designs[i]) {
+                    loadDesign(designs[i]);
+                    break;
+                    return designs[i];
+                }
+            }
+        });
+    }
+
+    function convert() {
+        session.convert("month" + m, function (err, res) {
+            if (err) throw err;
+        });
+    }
